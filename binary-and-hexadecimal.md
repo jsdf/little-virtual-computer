027723462fe2c522eb66dbff8924e958a1d2b65d
James Friend <james@jsdf.co>
2016-10-30 23:08:29 -0700

    initial commit

---
+++ binary-and-hexadecimal.md
@@ -0,0 +1,59 @@
+Before explaining how computers load data into their working space and process it, it's valuable to understand binary and hexadecimal numbers. This is because computer hardware only understands binary values due to the physical characteristics of the electronic circuitry used to implement them. I won't go further into explaining the reasons why computer hardware works with values in binary form, but you can read more about it [here](http://nookkin.com/articles/computer-science/why-computers-use-binary.ndoc).
+
+So what is binary? Binary is a 'base-2 number system'. But what does that mean?
+
+Consider the number system we are all accustomed to using in our everyday lives, which is sometimes called the decimal system or base-10. It uses the digits 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9 to represent the first 10 integers (whole-numbers) starting from zero. It is called base-10 because we have 10 digits to work with, from 0 to 9. But what happens after 9? With the number 10 we move over one column to the left, placing a '1' in the 'tens column' followed by a '0' in the 'ones column'. If we continue to increase our number in increments of 1, the digit in the 'ones column' moves through the digits 0-9, until we get to 20, and so on until we eventually get to 100, placing a 1 in the 'hundreds column'.
+
+Binary, or base-2, is much the same, except the only digits we have to work with are 0 and 1. Then how do we count? It's actually the same as in base-10, but after 0, then 1, we get to 10. Why? Because we have moved through all the digits we have to work with in the 'ones column', so we put a 1 in the next column to the left. However, in binary, that column is not the 'tens column', but rather the 'twos column'. In the same way that in the decimal number 20 we are basically saying that we have 'two tens and zero ones', in the binary number 10 we are saying that we have 'one twos and zero ones'. Next comes 11 (one twos and one ones) then 100 (one fours, zero twos, and zero ones).
+
+   0
+   1
+  10
+  11
+ 100
+ 101
+
+If it seems confusing that the columns, from right to left are 'ones', 'two', 'fours', rather than 'ones', 'tens', 'hundreds' consider that in base-10 we only need a tens column once we've exhausted all of the digits we can put in the ones column (0-9) once we reach the number 9, and the next whole number after 9 is 10, but in base-2 we only have 0 and 1, so after 0, then 1, we have exhausted all the digits for the ones column, and the next number we want to represent is the number that (in base-10) we would call 'two'. By calling it the 'twos column' we're still using the base-10 name for that number. It's valuable to understand that each number can be represented in both base-2 and base-10, or any other base for that matter, and the only difference is how we write them in digits (or however else we are recording them, such as in the two positions of a switch). As we continue on to larger and larger numbers we have the columns ones, twos, fours, eights, 16s, 32s, 64s and so on. You might recognise these as the powers of 2. 
+
+Hexadecimal (base-16) is much like binary and decimal, except that there are 16 digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, and f. After 9 we start using letters of the alphabet to fill out the remaining digits to bring us to a total of 16. This means that as we are counting, after 9 we don't go to 10, but instead a, then b, c, e, and finally f, before getting to 10. Instead of the column to the left of the ones column being the tens column, in hexadecimal it is the '16s column'. Once we have moved through 0 to f in that column (moving through 0 to f in the ones column for each digit in the 16s column), the next column is the 
+256s, 4096s, 65536s, and so on, moving up in the powers of 16. As the columns in binary (base-2) go up in powers of 2 as we move to the left, and the columns in decimal (base-10) in powers of 10, it makes sense that the columns in hexadecimal go up in powers of 16.
+
+   0           0    0
+   1           1    1
+   2          10    2
+   3          11    3
+   4         100    4
+   5         101    5
+   6         110    6
+   7         111    7
+   8        1000    8
+   9        1001    9
+   a        1010   10
+   b        1011   11
+   c        1100   12
+   d        1101   13
+   e        1110   14 
+   f        1111   15
+  10      1 0000   16
+  11      1 0001   17
+  12      1 0010   18
+  13      1 0011   19
+  14      1 0100
+  15      1 0101
+  16      1 0110
+  17      1 0111
+  18      1 1000
+  19      1 1001
+  1a      1 1010
+  1b      1 1011
+  1c      1 1100
+  1d      1 1101
+  1e      1 1110
+  1f      1 1111
+  20     10 0000
+ ...         ...
+ fd    1111 1101
+ fe    1111 1110
+ ff    1111 1111
+100  1 0000 0000
+101  1 0000 0001
---
+++ computer1.js
@@ -0,0 +1,188 @@
+
+// We are going to use an array to simulate computer memory. We can store a
+// Number value at each position in the array.
+const MEMORY = [];
+
+// The total amount of 'slots', or memory addresses, we are going to
+// have to store data values at.
+const TOTAL_MEMORY_SIZE = 1000;
+
+// As we move through our program, we need to keep track of where we are up to.
+// In a real computer, there is a small piece of memory inside the CPU which
+// holds this information, and the program counter contains a memory address 
+// pointing to the start of the program in memory. However for our simulation we
+// will be representing the program as an array of instructions, so this counter
+// keeps track of the array index of the instruction we're currently at.
+let programCounter = 0;
+
+// Our program is represented as an array, with each item itself being an array
+// which contains the instruction name, followed by the arguments to the
+// instruction, if any. We'll include a button in the user interface for loading
+// the program code.
+let program = [];
+
+// We'll keep a map of the 'label' instructions in the program so we can
+// jump to them later on. More on this later.
+const labelLocations = {};
+
+// Store a value at a certain address in memory
+function setMem(address, value) {
+  if (address < 0 || address >= TOTAL_MEMORY_SIZE) {
+    throw new Error('tried to write to an invalid memory address');
+  }
+  MEMORY[address] = value;
+}
+
+// Get the value which is stored at a certain address in memory
+function getMem(address) {
+  if (address < 0 || address >= TOTAL_MEMORY_SIZE) {
+    throw new Error('tried to read from an invalid memory address');
+  }
+  return MEMORY[address];
+}
+
+const instructions = {
+  // We expose our memory setting function to be used in our program so that the
+  // program code can initialize values in memory with whatever values they
+  // need.
+  'set_data': function(startAddress, data) {
+    let currentAddress = startAddress;
+    for (const value of data) {
+      setMem(currentAddress, data);
+      currentAddress++;
+    }
+  },
+  'set_value': function(address, value) {
+    setMem(address, value);
+  },
+  // add the value at the address 'a' with the value at the address 'b' and
+  // store them at the address 'out'
+  'add': function(aAddr, bAddr, outAddr) {
+    const aVal = getMem(aAddr);
+    const bVal = getMem(bAddr);
+    const outVal = aVal + bVal;
+    setMem(outAddr, outVal);
+  },
+  'label': function() {
+    // don't actually do anything for the 'label' instruction, it's just here to
+    // mark a place in the program so that we can jump to it as needed
+  },
+  'jump_to': function(labelName) {
+
+  },
+}
+
+// In a real computer, memory addresses which have never had any value set are
+// considered 'uninitialized', and might contain any garbage value, but to keep
+// our simulation simple we're going to initialize every location with the value
+// 0. However, just like in a real computer, in our simulation it is possible
+// for us to mistakenly read from the wrong place in memory if we have a bug in
+// our simulated program where we get the memory address wrong.
+for (var i = 0; i < TOTAL_MEMORY_SIZE; i++) {
+  MEMORY[i] = 0;
+}
+
+// We use a simple text-based language to input our program, which we will
+// convert into the array representation described earlier. This is our
+// equivalent of an assembly language.
+// We parse the program text into our array form by splitting the text into
+// lines, then splitting those lines into tokens (words), which gives us to an
+// instruction name and arguments for that instruction, from each line.
+function parseProgramText(programText) {
+  const programInstructions = [];
+  const lines = programText.split('\n');
+  for (const line of lines) {
+    const instruction = [];
+    let tokens = line.split(' ');
+    for (const token of tokens) {
+      // skip empty tokens
+      if (token == null || token == "") {
+        continue;
+      }
+      // first token
+      if (instruction.length == 0) {
+        instruction.push(token); // instruction name token
+      } else {
+        instruction.push(parseInt(token, 0)); // turn token text into Number value
+      }
+    }
+    //  if instruction was found on this line, add it to the program
+    if (instruction.length) {
+      programInstructions.push(instruction);
+    }
+  }
+  return programInstructions;
+}
+
+// Advance the program counter and run the instruction at the new position.
+// Returns true once the program is finished.
+function step() {
+  if (programCounter >= program.length) {
+    return true;
+  }
+  const instruction = program[programCounter];
+  const [name] = instruction;
+  const args = instruction.slice(1, instruction.length);
+  instructions[name].apply(null, args);
+  programCounter++;
+  return false;
+}
+
+function stepOnce() {
+  step();
+  updateOutput();
+}
+function runToEnd() {
+  let done = false;
+  while(!done) {
+    done = step();
+  }
+  updateOutput();
+}
+
+// boring code for rendering user interface of the simulator
+// not really important for understanding how computers work
+const stepperEl = document.getElementById('stepper');
+const memoryViewEl = document.getElementById('memoryView');
+const programCounterEl = document.getElementById('programCounter');
+
+function loadProgram() {
+  const programText = document.getElementById('program').textContent;
+  program = parseProgramText(programText);
+}
+
+const LINES_TO_PRINT = 20;
+function updateOutput() {
+  programCounterEl.value = programCounter;
+  updateStepper();
+  updateMemoryView();
+}
+
+function updateStepper() {
+  let stepperOutput = '';
+  const offset = -Math.floor(LINES_TO_PRINT / 2);
+  const start = Math.max(programCounter + offset, 0);
+  const end = Math.min(programCounter + offset + LINES_TO_PRINT, program.length - 1);
+  for (var i = start; i <= end; i++) {
+    const instruction = program[i];
+
+    stepperOutput += instruction.join(' ');
+    if (i == programCounter) {
+      stepperOutput += ' <--'
+    }
+    stepperOutput += '\n';
+  }
+
+  stepperEl.textContent = stepperOutput;
+}
+
+function updateMemoryView() {
+  memoryViewEl.textContent = MEMORY.map((v,i) => `${i}: ${v}`).join('\n');
+}
+
+function clamp(val, min, max) {
+  return Math.min(min, Math.max(max, val));
+}
+
+loadProgram();
+updateOutput();
