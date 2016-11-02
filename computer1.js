e478dd639944829f4e3f246625f20b2486f0daee
James Friend <james@jsdf.co>
2016-11-01 23:22:03 -0700

    implement assembler

--- computer1.js
+++ computer1.js
@@ -4,8 +4,13 @@
 const MEMORY = [];
 
 // The total amount of 'slots', or memory addresses, we are going to
-// have to store data values at.
+// have to store data values at. Real computers have memory which can be read as
+// individual bytes, as well as in larger or smaller increments, but we will be
+// simulating it using an array of Number values, which we'll access by index.
 const TOTAL_MEMORY_SIZE = 1000;
+
+// The program will be loaded into memory starting at this slot.
+const PROGRAM_START = 600;
 
 // As we move through our program, we need to keep track of where we are up to.
 // In a real computer, there is a small piece of memory inside the CPU which
@@ -70,7 +75,19 @@ const instructions = {
   'jump_to': function(labelName) {
 
   },
-}
+};
+
+// we'll set up a mapping between our instruction names and the values we will
+// compile them into (which will be interpreted by our simulated cpu as it runs
+// the program)
+const instructionsOpcodes = new Map();
+const opcodesInstructions = new Map();
+
+Object.keys(instructions).forEach((instructionName, index) => {
+  const opcode = index + 100; // opcodes start at 100
+  instructionsOpcodes.set(instructionName, opcode);
+  opcodesInstructions.set(opcode, instructionName);
+});
 
 // In a real computer, memory addresses which have never had any value set are
 // considered 'uninitialized', and might contain any garbage value, but to keep
@@ -114,6 +131,16 @@ function parseProgramText(programText) {
   return programInstructions;
 }
 
+function assembleAndLoadProgram(program) {
+  let loadingAddress = PROGRAM_START;
+  for (const instruction of program) {
+    MEMORY[loadingAddress++] = instructionsOpcodes.get(instruction[0]);
+    for (var i = 1; i < instruction.length; i++) {
+      MEMORY[loadingAddress++] = instruction[i];
+    }
+  }
+}
+
 // Advance the program counter and run the instruction at the new position.
 // Returns true once the program is finished.
 function step() {
@@ -128,13 +155,9 @@ function step() {
   return false;
 }
 
-function updateUI() {
-  UI.update(MEMORY, programCounter, program);
-}
-
 function stepOnce() {
   step();
-  updateUI();
+  updateUI(MEMORY, programCounter, program);
 }
 function runToEnd() {
   let done = false;
@@ -142,13 +165,73 @@ function runToEnd() {
     done = step();
   }
 
-  updateUI();
+  updateUI(MEMORY, programCounter, program);
 }
 
 function loadProgram() {
-  const programText = UI.getProgramText();
-  program = parseProgramText(programText);
+  assembleAndLoadProgram(parseProgramText(getProgramText()));
 }
 
-loadProgram();
-updateUI();
+
+function init() {
+  loadProgram();
+  updateUI(MEMORY, programCounter, program);
+}
+
+// boring code for rendering user interface of the simulator
+// not really important for understanding how computers work
+
+const LINES_TO_PRINT = 20;
+
+const stepperEl = document.getElementById('stepper');
+const memoryViewEl = document.getElementById('memoryView');
+const programCounterEl = document.getElementById('programCounter');
+const programEl = document.getElementById('program');
+
+function getProgramText() {
+  return programEl.textContent;
+}
+
+function updateUI(
+  memory,
+  programCounter,
+  program
+) {
+  programCounterEl.value = programCounter;
+  updateStepper(
+    programCounter,
+    program
+  );
+  updateMemoryView(memory);
+}
+
+function updateStepper(
+  programCounter,
+  program
+) {
+  let stepperOutput = '';
+  const offset = -Math.floor(LINES_TO_PRINT / 2);
+  const start = Math.max(programCounter + offset, 0);
+  const end = Math.min(programCounter + offset + LINES_TO_PRINT, program.length - 1);
+  for (var i = start; i <= end; i++) {
+    const instruction = program[i];
+
+    stepperOutput += instruction.join(' ');
+    if (i == programCounter) {
+      stepperOutput += ' <--'
+    }
+    stepperOutput += '\n';
+  }
+
+  stepperEl.textContent = stepperOutput;
+}
+
+function updateMemoryView(memory) {
+  memoryViewEl.textContent = memory.map((v,i) => `${i}: ${v}`).join('\n');
+}
+
+function clamp(val, min, max) {
+  return Math.min(min, Math.max(max, val));
+}
+
+init();
