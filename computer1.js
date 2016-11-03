69ac4b209a7afdab4c0a675a47027a48ff839bda
James Friend <james@jsdf.co>
2016-11-02 23:28:28 -0700

    implement pc/opcode execution

--- computer1.js
+++ computer1.js
@@ -1,34 +1,39 @@
 
 // We are going to use an array to simulate computer memory. We can store a
-// Number value at each position in the array.
+// number value at each position in the array, and we will use a number value to
+// access each slot in the array (we'll call these 'memory addresses').
+// 
+// Real computers have memory which can be read as individual bytes, as well as
+// in larger or smaller chunks, and their addresses and values are usually shown
+// as hexadecimal (base-16) form, but we are going to represent addresses and
+// values as decimal (base-10) numbers, so there's one less thing to know about
+// at this point.
 const MEMORY = [];
 
-// The total amount of 'slots', or memory addresses, we are going to
-// have to store data values at. Real computers have memory which can be read as
-// individual bytes, as well as in larger or smaller increments, but we will be
-// simulating it using an array of Number values, which we'll access by index.
+// Here we have the total amount of array slots (or memory addresses) we are
+// going to have at which to store data values, and program code too, because
+// at the hardware level, program code is just another form of data stored in
+// memory.
 const TOTAL_MEMORY_SIZE = 1000;
 
 // The program will be loaded into memory starting at this slot.
 const PROGRAM_START = 600;
 
 // As we move through our program, we need to keep track of where we are up to.
+// The program counter contains a memory address pointing to the location of the
+// program instruction we are currently executing.
 // In a real computer, there is a small piece of memory inside the CPU which
-// holds this information, and the program counter contains a memory address 
-// pointing to the start of the program in memory. However for our simulation we
-// will be representing the program as an array of instructions, so this counter
-// keeps track of the array index of the instruction we're currently at.
-let programCounter = 0;
+// holds this information, called the 'program counter'. The program counter is
+// one of several 'registers', which are basically tiny pieces of memory built
+// right into the CPU, which just hold one value at a time, but can be accessed
+// very quickly.
+let programCounter = PROGRAM_START;
 
-// Our program is represented as an array, with each item itself being an array
-// which contains the instruction name, followed by the arguments to the
-// instruction, if any. We'll include a button in the user interface for loading
-// the program code.
-let program = [];
-
-// We'll keep a map of the 'label' instructions in the program so we can
-// jump to them later on. More on this later.
-const labelLocations = {};
+// We also need to keep track of whether the CPU is running or not. The 'break'
+// instruction, which is like 'debugger' in Javascript, will be implemented by
+// setting this to false. We'll automatically put one at the end of any program
+// we load so the CPU knows where to stop.
+let running = false;
 
 // Store a value at a certain address in memory
 function setMem(address, value) {
@@ -46,47 +51,67 @@ function getMem(address) {
   return MEMORY[address];
 }
 
+// These instructions represent the things the CPU can be told to do. We
+// implement them here with code, but a real CPU would have circuitry
+// implementing each one of these possible actions, which include things like
+// loading data from memory, comparing it, operating on and combining it, and
+// storing it back into memory.
 const instructions = {
+  // TODO: figure out how to implement this in assembler
+  'set_data': function() {
+    // const startAddress = getNextProgramValue();
+    // const length = getNextProgramValue();
+    // let currentAddress = startAddress;
+    // for (var currentAddress = startAddress; currentAddress < length; currentAddress++) {
+    //   MEMORY[currentAddress]
+    // }
+    // for (const value of data) {
+    //   setMem(currentAddress, data);
+    //   currentAddress++;
+    // }
+  },
   // We expose our memory setting function to be used in our program so that the
   // program code can initialize values in memory with whatever values they
   // need.
-  'set_data': function(startAddress, data) {
-    let currentAddress = startAddress;
-    for (const value of data) {
-      setMem(currentAddress, data);
-      currentAddress++;
-    }
-  },
-  'set_value': function(address, value) {
+  'set_value': function() {
+    const address = getNextProgramValue();
+    const value = getNextProgramValue();
     setMem(address, value);
   },
   // add the value at the address 'a' with the value at the address 'b' and
   // store them at the address 'out'
-  'add': function(aAddr, bAddr, outAddr) {
-    const aVal = getMem(aAddr);
-    const bVal = getMem(bAddr);
-    const outVal = aVal + bVal;
-    setMem(outAddr, outVal);
+  'add': function() {
+    const aAddress = getNextProgramValue();
+    const bAddress = getNextProgramValue();
+    const outAddress = getNextProgramValue();
+    const aValue = getMem(aAddress);
+    const bValue = getMem(bAddress);
+    const outValue = aValue + bValue;
+    setMem(outAddress, outValue);
   },
   'label': function() {
     // don't actually do anything for the 'label' instruction, it's just here to
     // mark a place in the program so that we can jump to it as needed
   },
-  'jump_to': function(labelName) {
+  'jump_to': function() {
 
   },
+  'break': function() {
+    running = false;
+  }
 };
 
-// we'll set up a mapping between our instruction names and the values we will
-// compile them into (which will be interpreted by our simulated cpu as it runs
-// the program)
-const instructionsOpcodes = new Map();
-const opcodesInstructions = new Map();
+// we'll set up a mapping between our instruction names and the numerical values
+// we will turn them into when we assemble the program. It is these numerical
+// values which will be interpreted by our simulated CPU as it runs
+// the program.
+const instructionsToOpcodes = new Map();
+const opcodesToInstructions = new Map();
 
 Object.keys(instructions).forEach((instructionName, index) => {
   const opcode = index + 100; // opcodes start at 100
-  instructionsOpcodes.set(instructionName, opcode);
-  opcodesInstructions.set(opcode, instructionName);
+  instructionsToOpcodes.set(instructionName, opcode);
+  opcodesToInstructions.set(opcode, instructionName);
 });
 
 // In a real computer, memory addresses which have never had any value set are
@@ -98,6 +123,11 @@ Object.keys(instructions).forEach((instructionName, index) => {
 for (var i = 0; i < TOTAL_MEMORY_SIZE; i++) {
   MEMORY[i] = 0;
 }
+
+// We'll keep a map of the 'label' instructions in the program so we can
+// jump to them later on. More on this later.
+// TODO: implement labels in assembler
+const labelLocations = {};
 
 // We use a simple text-based language to input our program, which we will
 // convert into the array representation described earlier. This is our
@@ -128,41 +158,49 @@ function parseProgramText(programText) {
       programInstructions.push(instruction);
     }
   }
+  programInstructions.push(['break']);
   return programInstructions;
 }
 
-function assembleAndLoadProgram(program) {
+// Having parsed our program text into an array, with each item itself being an
+// array containing tokens representing the instruction name followed by the
+// arguments to the instruction, we need to turn those tokens into values we can
+// store in the computer's memory, and load them in there.
+function assembleAndLoadProgram(programInstructions) {
   let loadingAddress = PROGRAM_START;
-  for (const instruction of program) {
-    MEMORY[loadingAddress++] = instructionsOpcodes.get(instruction[0]);
+  for (const instruction of programInstructions) {
+    MEMORY[loadingAddress++] = instructionsToOpcodes.get(instruction[0]);
     for (var i = 1; i < instruction.length; i++) {
       MEMORY[loadingAddress++] = instruction[i];
     }
   }
 }
 
+function getNextProgramValue() {
+  return MEMORY[programCounter++];
+}
+
 // Advance the program counter and run the instruction at the new position.
 // Returns true once the program is finished.
 function step() {
-  if (programCounter >= program.length) {
-    return true;
+  const opcode = getNextProgramValue();
+  const instructionName = opcodesToInstructions.get(opcode);
+  if (!instructionName) {
+    throw new Error(`Unknown opcode '${opcode}'. Did you keep running past the end?`);
   }
-  const instruction = program[programCounter];
-  const [name] = instruction;
-  const args = instruction.slice(1, instruction.length);
-  instructions[name].apply(null, args);
-  programCounter++;
-  return false;
+  instructions[instructionName]();
 }
 
 function stepOnce() {
+  running = true;
   step();
+  running = false;
   updateUI(MEMORY, programCounter, program);
 }
 function runToEnd() {
-  let done = false;
-  while(!done) {
-    done = step();
+  running = true;
+  while(running) {
+    step();
   }
 
   updateUI(MEMORY, programCounter, program);
