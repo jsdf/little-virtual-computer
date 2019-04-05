81bf5cbb4ec99b76a91a016d1a4cb13e218e6728
James Friend <james@jsdf.co>
2019-04-05 00:04:00 -0700

    fix audio in stupid ass chrome

--- computer1.js
+++ computer1.js
@@ -1727,3 +1727,12 @@ Assembler.init();
 SimulatorUI.initScreen(Display.SCREEN_WIDTH, Display.SCREEN_HEIGHT, Display.SCREEN_PIXEL_SCALE);
 SimulatorUI.initUI();
 Simulation.loadProgramAndReset();
+
+// enable audio to work with chrome autoplay policy :'(
+if (!document.body) throw new Error('DOM not ready');
+function resumeAudio() {
+  if (!document.body) throw new Error('DOM not ready');
+  document.body.removeEventListener('click', resumeAudio);
+  Audio.audioCtx.resume()
+}
+document.body.addEventListener('click', resumeAudio);
